7fc9a74b8549156b21bd2a83b31606ea4eb4814f
OpenEnv Dev <dev@openenv.local>
2026-04-12 15:27:25 +0530

    EDA and statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 #sys module is used to get the details of the error like which line the error is coming from and which file the error is coming from
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()#first two are not imp but third exc_tb gives info about exception
