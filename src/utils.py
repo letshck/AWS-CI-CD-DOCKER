69b5b3e72c98a381c72206d36b60c3891c74d5f7
OpenEnv Dev <dev@openenv.local>
2026-04-18 23:54:58 +0530

    Model Trainer

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+#contains functionalisties that entire project can use
+
+import os
+import sys
+import pickle
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+from sklearn.metrics import r2_score
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok = True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
