838c90e2d826a4a8ffcf96d8973a3a74c05377dc
OpenEnv Dev <dev@openenv.local>
2026-04-20 19:29:52 +0530

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -15,15 +16,32 @@ def save_object(file_path,obj):
             pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            model_param = param[list(models.keys())[i]]
+            gs = GridSearchCV(model,model_param,cv = 3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             y_test_pred = model.predict(X_test)
             test_model_score = r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
